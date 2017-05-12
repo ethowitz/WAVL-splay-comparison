c5ee6dd3cc8691caa59498cc84d363366c162209
ethowitz <ethan.donowitz@gmail.com>
2017-05-12 18:26:27 -0400

    completed code

--- test_suite.py
+++ test_suite.py
@@ -2,8 +2,10 @@ import bst
 import wavl
 import splay
 import random
+import time
 
 MAX = 10000
+NUM_ROUNDS = 10
 
 def gen_accesses(n):
     accesses = []
@@ -23,29 +25,152 @@ def deletions(ds, tree):
     for d in ds:
         tree.remove(d)
 
-# TODO: add while loop, collect averages
+avg_uni_b = 0
+avg_pretty_uneven_b4 = 0
+avg_pretty_uneven_b10 = 0
+avg_very_uneven_b = 0
+avg_inter_b = 0
+avg_del_b = 0
 
-data = random.sample(range(MAX), 8000)
-dels = random.sample(data, 1000)
-even_accesses = gen_accesses(1000)
-uneven_accesses = [int(random.expovariate(0.001)) for i in range(1000)]
+avg_uni_w = 0
+avg_pretty_uneven_w4 = 0
+avg_pretty_uneven_w10 = 0
+avg_very_uneven_w = 0
+avg_inter_w = 0
+avg_del_w = 0
 
-b = bst.BST()
-w = wavl.WAVL()
-s = splay.Splay()
+avg_uni_s = 0
+avg_pretty_uneven_s4 = 0
+avg_pretty_uneven_s10 = 0
+avg_very_uneven_s = 0
+avg_inter_s = 0
+avg_del_s = 0
 
-# TODO: judge based on height, results are as expected
-insertions(data, b)
-deletions(dels, b)
-insertions(data, w)
-deletions(dels, w)
-insertions(data, s)
-deletions(dels, s)
-print(b.height())
-print(w.height())
-print(s.height())
+for i in range(NUM_ROUNDS):
+    data = random.sample(range(MAX), MAX)
+    dels = random.sample(data, 1000)
+    even_accesses = random.sample(data, MAX)
+    very_uneven_accesses = []
+    element = data[random.randint(0, MAX)]
+    for i in range(MAX):
+        very_uneven_accesses.append(element)
 
-# TODO: gen new data, add to new trees, time these
-accesses(uneven_accesses, s)
-accesses(uneven_accesses, w)
-accesses(uneven_accesses, b)
+    pretty_uneven_accesses4 = []
+    base = random.randint(0, MAX - 4)
+    for i in range(MAX):
+        pretty_uneven_accesses4.append(random.randint(base, base + 4))
+
+    pretty_uneven_accesses10 = []
+    base = random.randint(0, MAX - 10)
+    for i in range(MAX):
+        pretty_uneven_accesses10.append(random.randint(base, base + 10))
+
+    b = bst.BST()
+    w = wavl.WAVL()
+    s = splay.Splay()
+    insertions(data, b)
+    insertions(data, w)
+    insertions(data, s)
+
+    # uniform accesses
+    start = time.process_time()
+    accesses(even_accesses, b)
+    end = time.process_time()
+    avg_uni_b += end - start
+
+    start = time.process_time()
+    accesses(even_accesses, w)
+    end = time.process_time()
+    avg_uni_w += end - start
+
+    start = time.process_time()
+    accesses(even_accesses, s)
+    end = time.process_time()
+    avg_uni_s += end - start
+
+    # nonuniform accesses
+    start = time.process_time()
+    accesses(pretty_uneven_accesses4, b)
+    end = time.process_time()
+    avg_pretty_uneven_b4 += end - start
+
+    start = time.process_time()
+    accesses(pretty_uneven_accesses4, w)
+    end = time.process_time()
+    avg_pretty_uneven_w4 += end - start
+
+    start = time.process_time()
+    accesses(pretty_uneven_accesses4, s)
+    end = time.process_time()
+    avg_pretty_uneven_s4 += end - start
+
+    start = time.process_time()
+    accesses(pretty_uneven_accesses10, b)
+    end = time.process_time()
+    avg_pretty_uneven_b10 += end - start
+
+    start = time.process_time()
+    accesses(pretty_uneven_accesses10, w)
+    end = time.process_time()
+    avg_pretty_uneven_w10 += end - start
+
+    start = time.process_time()
+    accesses(pretty_uneven_accesses10, s)
+    end = time.process_time()
+    avg_pretty_uneven_s10 += end - start
+
+    start = time.process_time()
+    accesses(very_uneven_accesses, b)
+    end = time.process_time()
+    avg_very_uneven_b += end - start
+
+    start = time.process_time()
+    accesses(very_uneven_accesses, w)
+    end = time.process_time()
+    avg_very_uneven_w += end - start
+
+    start = time.process_time()
+    accesses(very_uneven_accesses, s)
+    end = time.process_time()
+    avg_very_uneven_s += end - start
+
+    # test cases for deletion
+    start = time.process_time()
+    deletions(dels, b)
+    end = time.process_time()
+    avg_del_b += end - start
+
+    start = time.process_time()
+    deletions(dels, w)
+    end = time.process_time()
+    avg_del_w += end - start
+
+    start = time.process_time()
+    deletions(dels, s)
+    end = time.process_time()
+    avg_del_s += end - start
+
+print("<<<<<<<< Uniform Access Results (in seconds) >>>>>>>>")
+print("BST: ", avg_uni_b / NUM_ROUNDS)
+print("WAVL: ", avg_uni_w / NUM_ROUNDS)
+print("Splay: ", avg_uni_s / NUM_ROUNDS)
+
+print("<<<<<<<< Single Element Queried Repeatedly Results (in seconds) >>>>>>>>")
+print("BST: ", avg_very_uneven_b / NUM_ROUNDS)
+print("WAVL: ", avg_very_uneven_w / NUM_ROUNDS)
+print("Splay: ", avg_very_uneven_s / NUM_ROUNDS)
+
+print("<<<<<<<< Same 4 Elements Queried Randomly (in seconds) >>>>>>>>")
+print("BST: ", avg_pretty_uneven_b4 / NUM_ROUNDS)
+print("WAVL: ", avg_pretty_uneven_w4 / NUM_ROUNDS)
+print("Splay: ", avg_pretty_uneven_s4 / NUM_ROUNDS)
+
+print("<<<<<<<< Same 10 Elements Queried Randomly (in seconds) >>>>>>>>")
+print("BST: ", avg_pretty_uneven_b10 / NUM_ROUNDS)
+print("WAVL: ", avg_pretty_uneven_w10 / NUM_ROUNDS)
+print("Splay: ", avg_pretty_uneven_s10 / NUM_ROUNDS)
+
+print("<<<<<<<< Deleting 1000 Elements (in seconds) >>>>>>>>")
+print("BST: ", avg_del_b / NUM_ROUNDS)
+print("WAVL: ", avg_del_w / NUM_ROUNDS)
+print("Splay: ", avg_del_s / NUM_ROUNDS)
--- wavl.py
+++ wavl.py
@@ -10,35 +10,34 @@ class WAVL(bst.BST):
         super(WAVL, self).__init__()
 
     def rank_diffs(self, node):
-        assert node != None
         ldiff = node.rank + 1
         rdiff = node.rank + 1
         if node.left:
             ldiff = node.rank - node.left.rank
-            assert ldiff >= 0
         if node.right:
             rdiff = node.rank - node.right.rank
-            assert rdiff >= 0
 
         return (ldiff, rdiff)
 
     def promote(self, node):
-        assert node != None
         node.rank += 1
 
     def demote(self, node):
-        assert node != None
         node.rank -= 1
 
     def insert_rebalance(self, node):
         parent_diffs = self.rank_diffs(node.parent)
         if parent_diffs == (0, 1) or parent_diffs == (1, 0):
             curr_node = node
-            while curr_node.parent and (self.rank_diffs(curr_node.parent) == (0, 1) or self.rank_diffs(curr_node.parent) == (1, 0)):
+            while (curr_node.parent and
+                   (self.rank_diffs(curr_node.parent) == (0, 1) or
+                    self.rank_diffs(curr_node.parent) == (1, 0))):
                 self.promote(curr_node.parent)
                 curr_node = curr_node.parent
 
-            if curr_node.parent and (self.rank_diffs(curr_node.parent) == (0, 2) or self.rank_diffs(curr_node.parent) == (2, 0)):
+            if (curr_node.parent and
+                (self.rank_diffs(curr_node.parent) == (0, 2) or
+                 self.rank_diffs(curr_node.parent) == (2, 0))):
                 if curr_node == curr_node.parent.left:
                     z = curr_node.parent
                     y = curr_node.right
@@ -46,28 +45,25 @@ class WAVL(bst.BST):
                         self.right_rotate(curr_node.parent)
                         self.demote(z)
                     else:
-                        assert curr_node.rank - y.rank == 1
                         self.left_rotate(y.parent)
                         self.right_rotate(y.parent)
                         self.promote(y)
+                        assert y.left != None or y.right != None
                         self.demote(curr_node)
                         self.demote(z)
                 else:
-                    assert curr_node == curr_node.parent.right
                     z = curr_node.parent
                     y = curr_node.left
                     if y == None or curr_node.rank - y.rank == 2:
                         self.left_rotate(curr_node.parent)
                         self.demote(z)
                     else:
-                        assert curr_node.rank - y.rank == 1
                         self.right_rotate(y.parent)
                         self.left_rotate(y.parent)
                         self.promote(y)
+                        assert y.left != None or y.right != None
                         self.demote(curr_node)
                         self.demote(z)
-        else:
-            assert parent_diffs == (1, 1)
 
     def insert(self, key):
         if self.root:
@@ -112,28 +108,19 @@ class WAVL(bst.BST):
 
     # adapted from CLRS edition 3, page 298
     def _remove(self, z):
-        produces_22leaf = False
-        n = None
-        m = None
         par = z.parent
-        produces_3child = False
-
-        # determine how to rebalance
-        if z.left == None and z.right == None and z.parent:
-            m = z.parent
-            if (z.parent.left == z and z.parent.right == None and z.parent.rank - z.rank == 1) or (z.parent.right == z and z.parent.left == None and z.parent.rank - z.rank == 1):
-                produces_22leaf = True
-        if z.parent and z.parent.rank - z.rank == 2:
-            produces_3child = True
+        zrank = z.rank
+        n = None
 
         if z.left == None:
-            self.transplant(z, z.right)
             n = z.right
+            self.transplant(z, z.right)
         elif z.right == None:
-            self.transplant(z, z.left)
             n = z.left
+            self.transplant(z, z.left)
         else:
             y = self.get_min(z.right)
+            n = y
             if y.parent != z:
                 self.transplant(y, y.right)
                 y.right = z.right
@@ -141,22 +128,14 @@ class WAVL(bst.BST):
             self.transplant(z, y)
             y.left = z.left
             y.left.parent = y
-            n = y
 
         if n:
-            n.rank = z.rank
-
-        if produces_22leaf:
-            assert m == par
-            assert self.rank_diffs(m) == (2, 2)
-            self.demote(m)
-            if m.parent and m.parent.rank - m.rank == 3:
-                self.deletion_rebalance(m, par)
-        elif produces_3child:
-            if n == None:
-                assert par.rank + 1 == 3
-            else:
-                assert par.rank - n.rank == 3
+            n.rank = zrank
+        if par and n and self.rank_diffs(n) == (2, 2):
+            self.demote(n)
+            if n.parent and n.parent.rank - n.rank == 3:
+                self.deletion_rebalance(n, par)
+        elif par and n and par.rank - n.rank == 3:
             assert par.left == n or par.right == n
             self.deletion_rebalance(n, par)
 
@@ -172,18 +151,23 @@ class WAVL(bst.BST):
         if x == par.left:
             y = par.right
         else:
+            assert x == par.right
             y = par.left
 
-        x_rank = 0;
+        x_rank = 0
         if x == None:
             x_rank = -1
         else:
             x_rank = x.rank
 
-        while par.rank - x_rank == 3 and (par.rank - y.rank == 2 or self.rank_diffs(y) == (2, 2)):
-            self.demote(par)
-            if par.rank - y.rank != 2:
+        while (par and par.rank - x_rank == 3 and
+              (par.rank - y.rank == 2 or self.rank_diffs(y) == (2, 2))):
+            if par.rank - y.rank == 2:
+                self.demote(par)
+            else:
+                self.demote(par)
                 self.demote(y)
+
             x = par
             par = par.parent
             if x == par.left:
@@ -191,28 +175,21 @@ class WAVL(bst.BST):
             else:
                 y = par.left
 
-            if x == None:
-                x_rank = -1
-            else:
-                x_rank = x.rank
-
         rd = self.rank_diffs(par)
         if rd == (1, 3) or rd == (3, 1):
-            assert self.rank_diffs(y) != (2, 2)
-
             if x == par.left:
                 z = par
                 v = y.left
                 w = y.right
 
-                if w.parent.rank - w.rank == 1:
-                    self.left_rotate(y.parent) #just y?
+                if w and z and y.rank - w.rank == 1:
+                    self.left_rotate(y.parent)
                     self.promote(y)
+                    assert y.left != None or y.right != None
                     self.demote(z)
                     if z.left == None and z.right == None:
                         self.demote(z)
-                else:
-                    assert w.parent.rank - w.rank == 2
+                elif w and v:
                     self.right_rotate(v.parent)
                     self.left_rotate(v.parent)
                     self.promote(v)
@@ -226,14 +203,14 @@ class WAVL(bst.BST):
                 v = y.right
                 w = y.left
 
-                if w.parent.rank - w.rank == 1:
-                    self.right_rotate(y.parent) #just y?
+                if w and z and y.rank - w.rank == 1:
+                    self.right_rotate(y.parent)
                     self.promote(y)
+                    assert y.left != None or y.right != None
                     self.demote(z)
                     if z.left == None and z.right == None:
                         self.demote(z)
-                else:
-                    assert w.parent.rank - w.rank == 2
+                elif w and v:
                     self.left_rotate(v.parent)
                     self.right_rotate(v.parent)
                     self.promote(v)
@@ -264,12 +241,16 @@ class WAVL(bst.BST):
         if node.left == None and node.right == None:
             return node.rank == 0
         elif node.left and node.right:
-            assert (node.rank - node.left.rank == 1 or node.rank - node.left.rank == 2)
-            assert (node.rank - node.right.rank == 1 or node.rank - node.right.rank == 2)
-            return (node.rank - node.left.rank == 1 or node.rank - node.left.rank == 2) and (node.rank - node.right.rank == 1 or node.rank - node.right.rank == 2) and self._validate(node.left) and self._validate(node.right)
+            return ((node.rank - node.left.rank == 1 or
+                     node.rank - node.left.rank == 2) and
+                    (node.rank - node.right.rank == 1 or
+                     node.rank - node.right.rank == 2) and
+                    self._validate(node.left) and self._validate(node.right))
         elif node.left:
-            assert (node.rank - node.left.rank == 1 or node.rank - node.left.rank == 2)
-            return (node.rank - node.left.rank == 1 or node.rank - node.left.rank == 2) and self._validate(node.left)
+            return ((node.rank - node.left.rank == 1
+                    or node.rank - node.left.rank == 2) and
+                   self._validate(node.left))
         else:
-            assert (node.rank - node.right.rank == 1 or node.rank - node.right.rank == 2)
-            return (node.rank - node.right.rank == 1 or node.rank - node.right.rank == 2) and self._validate(node.right)
+            return ((node.rank - node.right.rank == 1 or
+                    node.rank - node.right.rank == 2) and
+                    self._validate(node.right))
